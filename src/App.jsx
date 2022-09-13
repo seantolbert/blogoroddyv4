057f5ace0ca0eed0b4a147cae1ffcdf4ffa9c281
Sean Tolbert <stolbert4150@gmail.com>
2022-09-13 15:15:57 -0400

    successful render of blog titles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { useFetch } from "./hooks/useFetch";
+import useFetch from "./hooks/useFetch";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const url = "http://localhost:8000/api/v2/pages";
+  // const url = "http://localhost:8000/api/v2/pages/";
+  const url =
+    "http://localhost:8000/api/v2/pages/?type=blog.BlogPage&fields=title,date,description,body";
   const { data: posts, isLoading, error } = useFetch(url);
 
   if (isLoading) {
@@ -17,29 +16,12 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {posts && posts.map((post) => (
+          <p key={post.id}>{post.title}</p>)
+          )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          {posts && posts.map((post) => (
-            <p key={post.name}>{post.description}</p>
-          ))}
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
