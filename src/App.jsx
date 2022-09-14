e62ce23cc525d16116eef343ed6c980453a31cab
Sean Tolbert <stolbert4150@gmail.com>
2022-09-13 22:23:09 -0400

    successfully renders streamfield images

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import "./App.css";
 import useFetch from "./hooks/useFetch";
 
 function App() {
-  // const url = "http://localhost:8000/api/v2/pages/";
+  const imageUrl = "http://localhost:8000/api/v2/images/";
   const url =
-    "http://localhost:8000/api/v2/pages/?type=blog.BlogPage&fields=title,date,description,body";
+    "http://localhost:8000/api/v2/pages/?type=blog.BlogPage&fields=title,date,description,body,main_image";
   const { data: posts, isLoading, error } = useFetch(url);
+  const { imageData } = useFetch(imageUrl);
 
   if (isLoading) {
     return <div>its loading</div>;
@@ -18,9 +19,30 @@ function App() {
   return (
     <div>
       <div>
-        {posts && posts.map((post) => (
-          <p key={post.id}>{post.title}</p>)
-          )}
+        {posts &&
+          posts.map((post) => (
+            <div className="posts" key={post.id}>
+              <p className="title">
+                <a href={`http://localhost:8000/blog/${post.meta.slug}`}>
+                  {post.title}
+                </a>
+              </p>
+              <p className="description">{post.description}</p>
+              <img
+                src={`http://localhost:8000${post.main_image.meta.download_url}`}
+                alt="will it work"
+              />
+              {post.body &&
+                post.body.map((el) => (
+                  <div key={el.id}>
+                    <div dangerouslySetInnerHTML={{ __html: el.value }}></div>
+                    {el.type === "image" && <img src="http://localhost:8000/media/images/tolbertcologo_nKwcQ81.max-700x600.png
+                    " alt="streamfield image" />}
+                    {/* <img src={imageUrl + el.value  } alt="{imageData}" /> */}
+                  </div>
+                ))} 
+            </div>
+          ))}
       </div>
     </div>
   );
